523733333c8d3920f5042c7f915edb1244790d0e
tejudevops <saitejaswini8121@gmail.com>
2025-03-27 15:21:59 +0530

    Expense frontendend shell scripting

---
+++ mysql.sh
@@ -0,0 +1,12 @@
+dnf module disable mysql -y
+cp /etc/yum.repos.d/mysql.repo
+
+dnf module install mysql-community-server -y
+
+dnf module enable mysqld
+dnf module start mysqld
+
+mysql_secure_installation --set-root-pass ExpenseApp@1
+
+mysql 172.31.94.208 -uroot -pExpenseApp@1
+
