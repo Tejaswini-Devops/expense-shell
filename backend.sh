cc27a21730378d6c8bf0c2bd6d1c45df0b420bfb
tejudevops <saitejaswini8121@gmail.com>
2025-03-28 10:10:04 +0530

    Expense frontendend shell scripting

--- backend.sh
+++ backend.sh
@@ -1,42 +1,42 @@
-echo -e "\e[33m disable nodejs current version[0m"
+echo -e "\e[33m disable nodejs current version\e[0m"
 dnf module disable nodejs -y >/tmp/expense.log
 
-echo -e  "\e[33m enable nodejs latest version[0m"
+echo -e  "\e[33m enable nodejs latest version\e[0m"
 dnf module enable nodejs:18 -y >/tmp/expense.log
 
-echo -e  "\e[33m install nodejs version[0m"
+echo -e  "\e[33m install nodejs version\e[0m"
 dnf install nodejs -y >/tmp/expense.log
 
-echo -e  "\e[33m configure backend.service file[0m"
+echo -e  "\e[33m configure backend.service file\e[0m"
 cp backend.service /etc/systemd/system/backend.service >/tmp/expense.log
 
-echo -e  "\e[33m Add user[0m"
+echo -e  "\e[33m Add user\e[0m"
 useradd expense >/tmp/expense.log
 
-echo -e  "\e[33m remove the existing content in app directory when you run code again[0m"
+echo -e  "\e[33m remove the existing content in app directory when you run code again\e[0m"
 rm -rf /app   >/tmp/expense.log //reruning should not fail so we added this step
 
-echo -e  "\e[33m create directory[0m"
+echo -e  "\e[33m create directory\e[0m"
 mkdir /app >/tmp/expense.log
 
-echo -e  "\e[33m download the application content[0m"
+echo -e  "\e[33m download the application content\e[0m"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip >/tmp/expense.log
 cd /app
 
-echo -e  "\e[33m extract the content[0m"
+echo -e  "\e[33m extract the content\e[0m"
 unzip /tmp/backend.zip >/tmp/expense.log
 
-echo -e  "\e[33m install the dependencies[0m"
+echo -e  "\e[33m install the dependencies\e[0m"
 npm install >/tmp/expense.log
 
-echo -e  "\e[33m reload the systemd and start backend services.[0m"
+echo -e  "\e[33m reload the systemd and start backend services\e[0m"
 systemctl daemon reload >/tmp/expense.log
 systemctl enable backend >/tmp/expense.log
 systemctl restart backend >/tmp/expense.log
 
-echo -e  "\e[33m install mysql client[0m"
+echo -e  "\e[33m install mysql client\e[0m"
 dnf install mysql -y >/tmp/expense.log
 
-echo -e  "\e[33m load the schema[0m"
+echo -e  "\e[33m load the schema\e[0m"
 mysql -h mysql-dev.tejudevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql >/tmp/expense.log
 
