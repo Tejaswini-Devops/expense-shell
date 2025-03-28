c4f7a987aad05038cfbcf22be886aab1efe940d0
tejudevops <saitejaswini8121@gmail.com>
2025-03-28 10:00:49 +0530

    Expense frontendend shell scripting

--- backend.sh
+++ backend.sh
@@ -1,42 +1,30 @@
-echo disable nodejs current version
+echo -e "\e[33m disable nodejs current version[0m"
 dnf module disable nodejs -y
-
-echo enable nodejs latest version
+echo -e  "\e[33m enable nodejs latest version[0m"
 dnf module enable nodejs:18 -y
-
-echo install nodejs version
+echo -e  "\e[33m install nodejs version[0m"
 dnf install nodejs -y
-
-echo configure backend.service file
+echo -e  "\e[33m configure backend.service file[0m"
 cp backend.service /etc/systemd/system/backend.service
-
-echo Add user
+echo -e  "\e[33m Add user[0m"
 useradd expense
-
-echo remove the existing content in app directory when you run code again
+echo -e  "\e[33m remove the existing content in app directory when you run code again[0m"
 rm -rf /app    //reruning should not fail so we added this step
-
-echo create directory
+echo -e  "\e[33m create directory[0m"
 mkdir /app
-
-echo download the application content
+echo -e  "\e[33m download the application content[0m"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 cd /app
-
-echo extract the content
+echo -e  "\e[33m extract the content[0m"
 unzip /tmp/backend.zip
-
-echo install the dependencies
+echo -e  "\e[33m install the dependencies[0m"
 npm install
-
-echo reload the systemd and start backend services.
+echo -e  "\e[33m reload the systemd and start backend services.[0m"
 systemctl daemon reload
 systemctl enable backend
 systemctl restart backend
-
-echo install mysql client
+echo -e  "\e[33m install mysql client[0m"
 dnf install mysql -y
-
-echo load the schema
+echo -e  "\e[33m load the schema[0m"
 mysql -h mysql-dev.tejudevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
 
