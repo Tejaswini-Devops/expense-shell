245a8fe74b11e4d5c731d607b22ec61d7ab7b972
tejudevops <saitejaswini8121@gmail.com>
2025-03-29 15:42:32 +0530

    Expense frontendend shell scripting

--- common.sh
+++ common.sh
@@ -8,14 +8,18 @@ DIR=$1
 
 Head "remove the existing/default app content"
 rm -rf $1  &>>$log_file //reruning should not fail so we added this step
+echo $?
 
 Head "create Application directory"
 mkdir $1 &>>$log_file
+echo $?
 
 Head "download the application content"
 curl -o /tmp/${component}.zip https://expense-artifacts.s3.amazonaws.com/${component}.zip &>>$log_file
 cd $1
+echo $?
 
 Head "extract the content"
 unzip /tmp/${component}.zip &>>$log_file
 }
+echo $?
