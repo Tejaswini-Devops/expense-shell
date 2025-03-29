83042da922bd04c000659bcfd3d775b6f32a8ad9
tejudevops <saitejaswini8121@gmail.com>
2025-03-29 15:48:12 +0530

    Expense frontendend shell scripting

--- frontend.sh
+++ frontend.sh
@@ -2,16 +2,16 @@ source common.sh
 component=frontend
 
 Head "Install nginx web server"
-dnf install nginx -y
+dnf install nginx -y &>>$log_file
 echo $?
 
 Head "Configure frontend"
-cp expense.conf /etc/nginx/default.d/expense.conf
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
 echo $?
 
 App_Preq /usr/share/nginx/html
 
 Head "start nginx services"
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx &>>$log_file
+systemctl restart nginx &>>$log_file
 echo $?
