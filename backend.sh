075a45d6a27de714e80e28d252f217e03b50b952
tejudevops <saitejaswini8121@gmail.com>
2025-03-31 13:03:01 +0530

    Expense frontendend shell scripting

--- backend.sh
+++ backend.sh
@@ -9,7 +9,7 @@ rm -rf /app
 mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
 cd /app
-unzip backend.zip
+unzip /tmp/backend.zip
 npm install
 
 systemctl daemon-reload
