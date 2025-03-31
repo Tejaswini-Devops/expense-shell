b36fe0fdd8f9b8ddb931cee5acd058448eb16e6a
tejudevops <saitejaswini8121@gmail.com>
2025-03-31 13:54:34 +0530

    Expense frontendend shell scripting

--- backend.sh
+++ backend.sh
@@ -4,43 +4,55 @@ Head(){
 }
 Head "Disable existing Nodejs version"
 dnf module disable nodejs -y &>>$log_file
+echo $?
 
 Head "Enable Latest version of nodejs"
 dnf module enable nodejs:20 -y &>>$log_file
+echo $?
 
 Head "Install Nodejs"
 dnf install nodejs -y &>>$log_file
+echo $?
 
 Head "Configure Backend service file"
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
+echo $?
 
 Head "Add User"
 useradd expense &>>$log_file
+echo $?
 
 Head "Remove the existing content"
 rm -rf /app &>>$log_file
+echo $?
 
 Head "Create Directory"
 mkdir /app &>>$log_file
+echo $?
 
 Head "Download the Content to Directory"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>$log_file
 cd /app &>>$log_file
+echo $?
 
 Head "Extract the Application content"
 unzip /tmp/backend.zip &>>$log_file
 npm install &>>$log_file
+echo $?
 
 Head "Start and reload the services"
 systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
 systemctl start backend &>>$log_file
+echo $?
 
 Head "Install mysql client"
 dnf install mysql -y &>>$log_file
+echo $?
 
 Head "Load Schema"
 mysql -h mysql-dev.tejudevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
+echo $?
 
 
 
