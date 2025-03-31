278c042bcacccdecf3f61f60f074310d3501b003
tejudevops <saitejaswini8121@gmail.com>
2025-03-31 14:02:32 +0530

    Expense frontendend shell scripting

--- frontend.sh
+++ frontend.sh
@@ -1,28 +1,28 @@
 source common.sh
 Head " Install nginx server"
-dnf install nginx -y
+dnf install nginx -y &>>$log_file
 echo $?
 
 Head "Configure the configuration fle"
-cp expense.conf /etc/nginx/default.d/expense.conf
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
 echo $?
 
 Head "Remove the existing content"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>$log_file
 echo $?
 
 Head "Download the Application Content"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
-cd  /usr/share/nginx/html
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
+cd  /usr/share/nginx/html &>>$log_file
 echo $?
 
 Head "Extract the Application content"
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>$log_file
 echo $?
 
 Head " Start and enable Nginx Services"
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx &>>$log_file
+systemctl restart nginx &>>$log_file
 echo $?
 
 
