145f6a8cb1ad7ce6d2d5e6a0ecfa72a1ba26dad5
tejudevops <saitejaswini8121@gmail.com>
2025-03-31 12:30:43 +0530

    Expense frontendend shell scripting

--- frontend.sh
+++ frontend.sh
@@ -7,7 +7,7 @@ rm-rf /usr/share/nginx/html/*
 
 curl -o /tmp/frontend.zip frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 
-cd /usr/nginx/share/html
+cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
 cp expense.conf /etc/nginx/default.d/expense.conf
