38edc5eb948f764f9328a3973879b630144a4f05
tejudevops <saitejaswini8121@gmail.com>
2025-03-31 13:17:49 +0530

    Expense frontendend shell scripting

--- backend.sh
+++ backend.sh
@@ -1,14 +1,15 @@
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
+
 dnf install nodejs-y
+cp backend.service /etc/systemd/system/backend.service
 
 useradd expense
-
-cp backend.service /etc/systemd/system/backend.service
 rm -rf /app
 mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
 cd /app
+
 unzip /tmp/backend.zip
 npm install
 
@@ -18,7 +19,7 @@ systemctl start backend
 
 dnf install mysql -y
 
-mysql -h "172.31.83.139" -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h 172.31.83.139 -uroot -pExpenseApp@1 < /app/schema/backend.sql
 
 
 
