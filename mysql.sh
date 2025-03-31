8c575d9be1b875638bd28ee691930667bd143633
tejudevops <saitejaswini8121@gmail.com>
2025-03-31 14:07:12 +0530

    Expense frontendend shell scripting

--- mysql.sh
+++ mysql.sh
@@ -1,14 +1,24 @@
-dnf module disable mysql -y
+source common.sh
+Head "Disable Current Mysql located in system"
+dnf module disable mysql -y &>>$log_file
+echo $?
 
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+Head "Create repo file"
+cp mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
+echo $?
 
+Head "Install my sql 5.7 as per developer choice"
+dnf install mysql-community-server &>>$log_file
+echo $?
 
-dnf install mysql-community-server
+Head "Enable and restart services"
+systemctl enable mysqld &>>$log_file
+systemctl start mysqld &>>$log_file
+echo $?
 
-systemctl enable mysqld
-systemctl start mysqld
-
-mysql_secure_installation --set-root-pass ExpenseApp@1
+Head "Change the default password"
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$log_file
+echo $?
 
 
 
